b7df1e34cf0e9935085d17d348020e9ee1fcaf56
markstanl <markgstanley1@gmail.com>
2024-02-06 14:37:46 -0600

    2024/2/6 2:36PM
    added tailwind css to the project. tailwind components should be stored in the tailwind.css file.

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import './tailwind.css';
+
+function App() {
+  return (
+    <div className="text-center text-blue-500">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
