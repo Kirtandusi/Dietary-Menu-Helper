98c50dbd5f88b089262d81f8b9b3f0fb8b50b3a3
Kirtan Dusi <67548970+Kirtandusi@users.noreply.github.com>
2024-02-06 17:39:18 -0600

    new commit 2024:02:06 5:39 CST

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css';
 import './tailwind.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members")
+   .then(res => res.json())
+   .then(data => setData(data))
+   console.log(data);
+  }, [])
+
+
+
+
   return (
     <div className="text-center text-blue-500">
       <h1>Hello, world!</h1>
