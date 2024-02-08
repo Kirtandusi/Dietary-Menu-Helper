28e0affb97d976198dfab5991b1f87e1f2c5c579
Kirtan Dusi <67548970+Kirtandusi@users.noreply.github.com>
2024-02-07 21:31:03 -0600

    a bunch of fwicken changes 2024:07:02 9:30PM CST

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,37 @@
 import './App.css';
 import './tailwind.css';
-import React from 'react';
+//import React from 'react';
 import WebRouter from './WebRouter';
-
+//import React, { useEffect, useState } from 'react';
 function App() {
-
+  //const [menus, setMenus] = useState([]);
   return (
     <>
       <WebRouter />
     </>
   );
+  // useEffect(() => {
+  //   // Fetch the menus from your Flask backend
+  //   fetch('http://localhost:5000/menu')
+  //     .then(response => response.json())
+  //     .then(data => setMenus(data))
+  //     .catch(error => console.error('Error:', error));
+  // }, []);
+
+  // return (
+  //   <div>
+  //     {menus.map((menu, index) => (
+  //       <div key={index}>
+  //         <h2>{menu.date}</h2>
+  //         <ul>
+  //           {menu.items.map((item, itemIndex) => (
+  //             <li key={itemIndex}>{item.name}: {item.description}</li>
+  //           ))}
+  //         </ul>
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 }
 
 export default App;
